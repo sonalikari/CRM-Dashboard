5a7b0d6450deb08882d2b3e4f3f4557c876d0935
sonalikari <sonali211103@gmail.com>
2025-02-10 17:57:12 +0530

    completed

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,10 @@
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", 
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}", 
+  ],
   theme: {
     extend: {},
   },
